03696908a78d1bb84f868310fc457a0c778214ad
OlehBabyak <babyakwork@gmail.com>
2024-12-01 16:09:10 +0200

    save

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,5 +15,11 @@ module.exports = {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
     },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: [
+        `0x86e185d0c18b2d200df7cbdc097fedbe396524c67eaedba4e6e297b06ca0cd69`,
+      ],
+    },
   },
 };
